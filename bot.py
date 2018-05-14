fc27233534197781648855a60c9cacf3314f251d
LeonardMichalas <leonard.michalas@hpe.com>
2018-05-14 02:59:59 +0200

    added Elastic Net Modell

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 import csv #allows to reat data from excel sheet
 import numpy as np #allows to perform calculation on our data
 from sklearn.svm import SVR  #allows us to build a predictive model
+from sklearn.linear_model import ElasticNet
 import matplotlib.pyplot as plt #allows to plot our data
 import threading
 from threading import Thread
@@ -14,11 +15,14 @@ dates = []
 prices = []
 score = 0
 
-#defining the support vector regression models
+#initialize modells
 svr_lin = SVR(kernel = 'linear', C = 1e3)
 svr_poly = SVR(kernel = 'poly', C = 1e3, degree = 2)
 svr_rbf = SVR(kernel = 'rbf', C = 1e3, gamma = 0.1) 
 svr_sig = SVR(kernel = 'sigmoid')
+
+reg_elastic = ElasticNet(random_state=0)
+
 
 #function that allows you to save data from a csv file to an array
 def read_data(file, i):
@@ -109,6 +113,7 @@ def train(TrainDates, TrainPrices):
     t2 = threading.Thread(target=svr_lin.fit, args=(TrainDates, TrainPrices))
     t3 = threading.Thread(target=svr_poly.fit, args=(TrainDates, TrainPrices))
     t4 = threading.Thread(target=svr_sig.fit, args=(TrainDates, TrainPrices))
+    t5 = threading.Thread(target=reg_elastic.fit, args=(TrainDates, TrainPrices))
     
     #Starts a thread for each modell, so that they get computed simuntainously
     t1.start()
@@ -119,6 +124,9 @@ def train(TrainDates, TrainPrices):
     print('t3 started')
     t4.start()
     print('t4 started')
+    t5.start()
+    print('t5 started')
+
     
     #Synchronize the threads
     t1.join()
@@ -129,6 +137,9 @@ def train(TrainDates, TrainPrices):
     print('t3 done...')
     t4.join()
     print('t4 done...')
+    t5.join()
+    print('t5 done...')
+
     return
 
 #function that tests your train modell with the test data
@@ -140,11 +151,13 @@ def test(TestDates, TestPrices):
     poly_test_predictions = []
     rbf_test_predicitions = []
     sig_test_predicitions = []
+    elastic_test_predictions = []
 
     lin_test_prediction = 0
     poly_test_prediction = 0
     rbf_test_predicition = 0
     sig_test_predicition = 0
+    elastic_test_prediction = 0
 
     for date in TestDates:
 
@@ -152,13 +165,15 @@ def test(TestDates, TestPrices):
         poly_test_prediction = svr_poly.predict(date)[0]
         rbf_test_predicition =  svr_rbf.predict(date)[0]
         sig_test_predicition = svr_sig.predict(date)[0]
+        elastic_test_prediction = reg_elastic.predict(date)[0]
 
         lin_test_predictions.append([date, lin_test_prediction])
         poly_test_predictions.append([date, poly_test_prediction])
         rbf_test_predicitions.append([date, rbf_test_predicition])
         sig_test_predicitions.append([date, sig_test_predicition])
+        elastic_test_predictions.append([date, elastic_test_prediction])
 
-    return lin_test_predictions, poly_test_predictions, rbf_test_predicitions, sig_test_predicitions 
+    return lin_test_predictions, poly_test_predictions, rbf_test_predicitions, sig_test_predicitions, elastic_test_predictions 
 
 #function that allows you to predict the data of the future, based on a day and the modell you want to use
 def predict(x, modell):
@@ -177,7 +192,11 @@ def predict(x, modell):
 
     if modell == 'sig':
         sig_prediction = svr_sig.predict(x)[0]
-        return sig_prediction    
+        return sig_prediction  
+
+    if modell == 'elastic':
+        elastic_prediction = reg_elastic.predict(x)[0]
+        return elastic_prediction  
 
     return   
 
@@ -190,10 +209,11 @@ def plot(dates, prices):
     plt.plot(dates, svr_rbf.predict(dates), color= 'red', label= 'RBF model') # plotting the line made by the RBF kernel
     plt.plot(dates,svr_lin.predict(dates), color= 'green', label= 'Linear model') # plotting the line made by linear kernel
     plt.plot(dates,svr_poly.predict(dates), color= 'blue', label= 'Polynomial model') # plotting the line made by polynomial kernel
-    plt.plot(dates,svr_sig.predict(dates), color= 'purple', label= 'Sigmoid model') # plotting the line made by polynomial kernel
+    plt.plot(dates,svr_sig.predict(dates), color= 'purple', label= 'Sigmoid model') # plotting the line made by sigmoid kernel
+    plt.plot(dates,reg_elastic.predict(dates), color= 'yellow', label= 'Elastic model') # plotting the line made by reg elastic
     plt.xlabel('Day')
     plt.ylabel('Price')
-    plt.title('Support Vector Regression')
+    plt.title('Trading Bot')
     plt.legend()
     plt.show()
     return 
@@ -231,7 +251,7 @@ def prediction_difference_avg (test_predictions):
         
     return difference/counter #avg difference
 
-def prediction_difference_avg_for_multiple (lin_test_predictions, poly_test_predictions, rbf_test_predictions, sig_test_predictions):
+def prediction_difference_avg_for_multiple (lin_test_predictions, poly_test_predictions, rbf_test_predictions, sig_test_predictions, elastic_test_predictions):
 
     que = Queue.Queue(4) #Needed to save results from the threads
 
@@ -239,6 +259,7 @@ def prediction_difference_avg_for_multiple (lin_test_predictions, poly_test_pred
     t2 = Thread(target=lambda q, arg1: q.put(prediction_difference_avg(poly_test_predictions, )), args=(que, 'Threads'))
     t3 = Thread(target=lambda q, arg1: q.put(prediction_difference_avg(rbf_test_predictions, )), args=(que, 'Threads'))
     t4 = Thread(target=lambda q, arg1: q.put(prediction_difference_avg(sig_test_predictions, )), args=(que, 'Threads'))
+    t5 = Thread(target=lambda q, arg1: q.put(prediction_difference_avg(elastic_test_predictions, )), args=(que, 'Threads'))
 
     #Starts a thread for each modell, so that they get computed simuntainously
     t1.start()
@@ -249,6 +270,8 @@ def prediction_difference_avg_for_multiple (lin_test_predictions, poly_test_pred
     print('t3 started')
     t4.start()
     print('t4 started')
+    t5.start()
+    print('t5 started')    
     
     #Synchronize the threads
     t1.join()
@@ -259,7 +282,8 @@ def prediction_difference_avg_for_multiple (lin_test_predictions, poly_test_pred
     print('t3 done... Rbf Average: ', que.get())
     t4.join()
     print('t4 done... Sig Average: ', que.get())
-
+    t5.join()
+    print('t5 done... Sig Average: ', que.get())
     return
 
 #function calls    
@@ -290,12 +314,13 @@ print('TestDates: ',TestDates)
 train(TrainDates, TrainPrices)
 
 #test the model with the test data set and print results
-lin_test_predictions, poly_test_predictions, rbf_test_predicitions, sig_test_predicitions = test(TestDates, TestPrices)
+lin_test_predictions, poly_test_predictions, rbf_test_predicitions, sig_test_predicitions, elastic_test_predictions = test(TestDates, TestPrices)
 
 print('Lin: ',lin_test_predictions)    
 print('Poly: ',poly_test_predictions) 
 print('Rbf: ',rbf_test_predicitions)
 print('Sig: ',sig_test_predicitions)
+print('Elastic: ',elastic_test_predictions)
 
 #make predictions for the future with different models
 prediction_with_lin = predict(1.1, 'lin')
@@ -310,8 +335,11 @@ print('Rbf prediction:', prediction_with_rbf)
 prediction_with_sig = predict(1.1, 'sig')
 print('Sig prediction:', prediction_with_sig)
 
+prediction_with_elastic = predict(1.1, 'elastic')
+print('Elastic prediction:', prediction_with_elastic)
+
 #calcutate the difference between the predicted and the actual data for single data point 
-singleDataPoint = np.take(dates, 20) 
+singleDataPoint = np.take(dates, 19) 
 
 print('Single Data Point:', singleDataPoint)
 
@@ -327,9 +355,12 @@ print('Rbf difference (prediction - acutal):', difference_with_rbf)
 difference_with_sig = prediction_difference(singleDataPoint, 'sig')
 print('Sig difference (prediction - acutal):', difference_with_sig)
 
+difference_with_elastic = prediction_difference(singleDataPoint, 'elastic')
+print('Elastic difference (prediction - acutal):', difference_with_elastic)
+
 #calcutate the average difference between the predicted and the actual data for test set for diff modells
 
-prediction_difference_avg_for_multiple(lin_test_predictions, poly_test_predictions, rbf_test_predicitions, sig_test_predicitions)
+prediction_difference_avg_for_multiple(lin_test_predictions, poly_test_predictions, rbf_test_predicitions, sig_test_predicitions, elastic_test_predictions)
 
 #Debrecated... 
 #print ("average difference with lin", prediction_difference_avg(lin_test_predictions))
